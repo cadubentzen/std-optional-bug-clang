8d612e3ae0254caedd9a51e7ccdf3219a8e74abc
Carlos Bentzen <cadubentzen@igalia.com>
2024-07-13 12:29:09 +0200

    replace ternary with if

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main() {
     int v1 = 1;
     int v2 = takeIntRefReturn0(v1);
     v3 = v2 != 0;
-    v4 = v3 ? v1 : v4;
+    if (v3)
+      v4 = v1;
   }
   if (logicalAnd(v3, v4 > 0))
     assertNotReached(v4);
