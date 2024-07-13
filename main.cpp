4b9beb12c5bfa4e304e75d09a2aa2c95298b8c33
Carlos Bentzen <cadubentzen@igalia.com>
2024-07-13 11:17:18 +0200

    reduce main.cpp further

--- main.cpp
+++ main.cpp
@@ -1,38 +1,19 @@
-// Defining `incrementIntIfStringsEqual` in the same compilation unit fixes the
-// bug, it needs to be in a separate one.
 int takeIntRefReturn0(int &);
-void assertNotReached(int a);
+void assertNotReached(int);
 
-// Very stripped-down version of std::optional from libstdc++.
-// The minimum to reproduce the issue.
-class OptInt {
-public:
-  // Fixes the issue by replacing the empty ctor with:
-  // constexpr OptInt() = default;
-  constexpr OptInt() {}
-  constexpr OptInt(int t) : payload(t), engaged(true) {}
-
-  constexpr const int &operator*() const & noexcept { return this->payload; }
-  constexpr explicit operator bool() const noexcept { return this->engaged; }
-
-private:
-  int payload;
-  bool engaged{false};
-};
-
-// Also fixes the issue by adding __attribute__((noinline)
-inline OptInt shouldReturnEmptyOptional() {
-  int v = 1;
-  if (takeIntRefReturn0(v))
-    return v;
-  return {};
-}
+static bool logicalAnd(bool a, bool b) { return a && b; }
 
 int main() {
-  auto opt = shouldReturnEmptyOptional();
-  // The issue is gone if `*opt > 0` is removed.
-  if (opt && *opt > 0) {
-    // If opt is not dereferenced inside this scope, the bug is also gone
-    assertNotReached(*opt);
+  bool v3;
+  int v4;
+  {
+    int v1 = 1;
+    int v2 = takeIntRefReturn0(v1);
+    v3 = v2 != 0;
+    v4 = v3 ? v1 : v4;
   }
+  if (logicalAnd(v3, v4 > 0))
+    assertNotReached(v4);
+
+  return 0;
 }
